cd4c479145579bc96e9191893d5b6d3cbfdc6703
Rabi-anando-sarkar <rabianandos2002@gmail.com>
2025-05-30 20:16:08 +0530

    Island Perimeter

---
+++ Class-five/Island_perimeter_v1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int islandPerimeter(vector<vector<int>>& iGrid) {
+    int perimeter = 0;
+    int rows = iGrid.size();
+    int cols = iGrid[0].size();
+
+    for(int i = 0; i<rows; i++) {
+        for(int j=0; j<cols; j++) {
+            if(iGrid[i][j] == 1) {
+                perimeter+=4;
+                if(i < rows-1 && iGrid[i+1][j] == 1) {
+                    perimeter-=2;
+                }
+                if(j < cols-1 && iGrid[i][j+1] == 1) {
+                    perimeter-=2;
+                }
+            }
+        }
+    }
+
+    return perimeter;
+}
+
+int main() {
+    vector<vector<int>> grid = {
+        {0, 1, 0, 0},
+        {1, 1, 1, 0},
+        {0, 1, 0, 0},
+        {1, 1, 0, 0}
+    };
+
+    int result = islandPerimeter(grid);
+
+    cout << "Perimeter of the island is : " << result << endl;
+
+    return 0;
+}
