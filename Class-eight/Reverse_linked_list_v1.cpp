a2f14e04c454fc773d77a6fe8995c70f0ca5dad2
Rabi-anando-sarkar <rabianandos2002@gmail.com>
2025-06-09 11:50:00 +0530

    Reverse linked list

---
+++ Class-eight/Reverse_linked_list_v1.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) {
+        val = x,
+        next = nullptr;
+    }
+};
+
+class Solution {
+    public:
+        void display(ListNode *head) {
+            ListNode *temp;
+            temp = head;
+            while(temp!=nullptr) {
+                cout << temp -> val << " -> ";
+                temp = temp->next;
+            }
+        }
+
+        ListNode* reverseLinkedList(ListNode *head) {
+            ListNode *temp = nullptr;
+
+            if(head == nullptr) return head;
+            if(head->next == nullptr) return head;
+
+            ListNode *next = nullptr;
+
+            while(head != nullptr) {
+                next=head->next;
+                head->next=temp;
+                temp=head;
+                head=next;
+            }
+
+            return temp;
+        }
+};
+
+int main() {
+    ListNode *n1 = new ListNode(1);
+    ListNode *n2 = new ListNode(2);
+    ListNode *n3 = new ListNode(3);
+    ListNode *n4 = new ListNode(4);
+    ListNode *n5 = new ListNode(5);
+
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = n4;
+    n4->next = n5;
+
+    Solution sol;
+
+    sol.display(n1);
+    
+    ListNode *result = sol.reverseLinkedList(n1);
+    cout << '\n';
+    sol.display(result);
+
+    delete n1;
+    delete n2;
+    delete n3;
+    delete n4;
+    delete n5;
+
+    return 0;
+}
