e6021d0bdfbc473b30260416d16af012fe852f6b
Rabi-anando-sarkar <rabianandos2002@gmail.com>
2025-06-14 13:01:08 +0530

    Tree

--- Class-ten/Tree.cpp
+++ Class-ten/Tree.cpp
@@ -2,12 +2,69 @@
 
 using namespace std;
 
+class TreeNode {
+    public:
+        int value;
+        TreeNode* left;
+        TreeNode* right;
+
+        TreeNode(int v) {
+            value = v;
+            left = nullptr;
+            right = nullptr;
+        }
+};
+
 class Tree {
     private:
-
+        
     public:
+        void preOrder(TreeNode* root) {
+            if(root != nullptr) {
+                cout << root->value << " ";
+                preOrder(root->left);
+                preOrder(root->right);
+            }
+        }
+        void inOrder(TreeNode* root) {
+            if(root != nullptr) {
+                preOrder(root->left);
+                cout << root->value << " ";
+                preOrder(root->right);
+            }
+        }
+        void postOrder(TreeNode* root) {
+            if(root != nullptr) {
+                preOrder(root->left);
+                preOrder(root->right);
+                cout << root->value << " ";
+            }
+        }
 };
 
 int main() {
+    // Level 0
+    TreeNode* root = new TreeNode(10);
+
+    // Level 1
+    root->left = new TreeNode(20);
+    root->right = new TreeNode(30);
+
+    // Level 2
+    root->left->left = new TreeNode(40);
+    root->left->right = new TreeNode(50);
+    root->right->left = new TreeNode(60);
+    root->right->right = new TreeNode(70);
+
+    Tree t;
+
+    t.preOrder(root);
+    cout << '\n';
+
+    t.inOrder(root);
+    cout << '\n';
+
+    t.postOrder(root);
+
     return 0;
 }
